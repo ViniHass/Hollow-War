189fb909bfae3a362b86743ec38f42a8c6290632
CarlosjuniorBr1 <carlos.alberto369@outlook.com>
2025-11-11 22:06:39 -0300

    Barra de vida

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -6,14 +6,23 @@ public class Health : MonoBehaviour
     [SerializeField] private int maxHealth = 20;
     private int currentHealth;
 
+    public Transform healthBar; // barra verde
+    public GameObject healthBarObject; // objeto pai das barras
+
+
+    private Vector3 healthBarScale;
+    private float healthPercent;
     void Start()
     {
         currentHealth = maxHealth;
+        healthBarScale = healthBar.localScale;
+        healthPercent = healthBarScale.x / currentHealth;
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        UpdateHealthbar();
         Debug.Log(gameObject.name + " tomou " + damage + " de dano. Vida restante: " + currentHealth);
 
         if (currentHealth <= 0)
@@ -29,10 +38,17 @@ public class Health : MonoBehaviour
         // Mais tarde, você pode adicionar animações de morte, efeitos, etc.
 
         gameObject.SetActive(false);
-
+         
         SceneManager.LoadScene("Overworld");
 
         // Carrega a segunda cena sem descarregar a primeira
         SceneManager.LoadScene("Pradaria", LoadSceneMode.Additive);
+    }
+
+
+    void UpdateHealthbar()
+    {
+        healthBarScale.x = healthPercent*currentHealth;
+        healthBar.localScale = healthBarScale;
     }
 }
